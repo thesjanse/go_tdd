dab7d2fdd3f0a3a262a84f7043785fc77b5df840
Alexander Mejinski <alexander.mejinski@gmail.com>
2022-12-06 16:15:04 +0300

    feature: add empty string argument support for Hello function

--- 01_hello_world/hello_go_test.go
+++ 01_hello_world/hello_go_test.go
@@ -2,11 +2,23 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Jane")
-	want := "Hello, Jane!"
-
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("say hello to a person", func(t *testing.T) {
+		got := Hello("Jane")
+		want := "Hello, Jane!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello with an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
 }
