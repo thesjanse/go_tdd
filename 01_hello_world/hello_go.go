9976d44386b0f333b2bcae0407432113e8d0c31c
Alexander Mejinski <alexander.mejinski@gmail.com>
2022-12-27 23:59:57 +0300

    feature: add greetingPrefix switch function

--- 01_hello_world/hello_go.go
+++ 01_hello_world/hello_go.go
@@ -2,13 +2,7 @@ package main
 
 import "fmt"
 
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := "Hello, "
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "Spanish":
 		prefix = "Hola, "
@@ -16,9 +10,18 @@ func Hello(name string, language string) string {
 		prefix = "Bonjour, "
 	case "Latin":
 		prefix = "Salve, "
+	default:
+		prefix = "Hello, "
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
 	}
 
-	return prefix + name
+	return greetingPrefix(language) + name
 }
 
 func main() {
