e9bf72c580340180f512f1b6fa7fe3df5366b631
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-14 09:32:06 +0300

    refactor: rename function parameters

--- 03_iteration/iteration.go
+++ 03_iteration/iteration.go
@@ -1,10 +1,10 @@
 package iteration
 
-// repeats character n times and returns result as string
-func Repeat(char string, n int) string {
+// repeats string n times and returns result as string
+func Repeat(str string, n int) string {
 	var result string
 	for i := 0; i < n; i++ {
-		result += char
+		result += str
 	}
 	return result
 }
