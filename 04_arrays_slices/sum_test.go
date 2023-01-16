ea3e40fa9404140acbf0d14339072e1a1ceb1e8b
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-16 22:59:36 +0300

    refactor: remove redundant test

--- 04_arrays_slices/sum_test.go
+++ 04_arrays_slices/sum_test.go
@@ -10,12 +10,6 @@ func assertCorrectMessage(t testing.TB, actual, expected int, numbers []int) {
 }
 
 func TestSum(t *testing.T) {
-	t.Run("collection of five elements", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		actual := Sum(numbers)
-		expected := 15
-		assertCorrectMessage(t, actual, expected, numbers)
-	})
 
 	t.Run("collection of 3 elements", func(t *testing.T) {
 		numbers := []int{10, 20, 33}
