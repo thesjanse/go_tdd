bdfa91aa97ce5ae522f7aa4fbb4b21cbdc0d97f6
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-20 22:45:27 +0300

    refactor: methods instead of functions; feature: area of circle

--- 05_structs_methods_interfaces/smi.go
+++ 05_structs_methods_interfaces/smi.go
@@ -1,14 +1,24 @@
 package smi
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(r Rectangle) float64 {
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
 	return (r.width + r.height) * 2
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
 }
