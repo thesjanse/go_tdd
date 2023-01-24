0a51022fca13bcfc953fe63f086c8792d9735108
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-24 23:54:03 +0300

    refactor: assert into Wallet.Balance()

--- 06_pointers_errors/pe_test.go
+++ 06_pointers_errors/pe_test.go
@@ -3,8 +3,9 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assert := func(t testing.TB, actual, expected Bitcoin) {
+	assert := func(t testing.TB, wallet Wallet, expected Bitcoin) {
 		t.Helper()
+		actual := wallet.Balance()
 		if actual != expected {
 			t.Errorf("Actual: '%s'; Expected: '%s'", actual, expected)
 		}
@@ -13,18 +14,12 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit to empty wallet", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-
-		actual := wallet.Balance()
-		expected := Bitcoin(10)
-		assert(t, actual, expected)
+		assert(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw from non-empty wallet", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(10)}
 		wallet.Withdraw(Bitcoin(10))
-
-		actual := wallet.Balance()
-		expected := Bitcoin(0)
-		assert(t, actual, expected)
+		assert(t, wallet, Bitcoin(0))
 	})
 }
