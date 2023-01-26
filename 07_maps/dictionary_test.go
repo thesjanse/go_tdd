5a3fde32f66f22a77b516bfd8525606e008db124
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-26 23:22:11 +0300

    refactor: add assertStrings helper function

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -7,8 +7,14 @@ func TestSearch(t *testing.T) {
 		d := map[string]string{"test": "simple test"}
 		actual := Search(d, "test")
 		expected := "simple test"
-		if actual != expected {
-			t.Errorf("Expected: '%s' but actual: '%s'", expected, actual)
-		}
+		assertStrings(t, actual, expected)
 	})
+}
+
+func assertStrings(t testing.TB, actual, expected string) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("Expected: '%s' but actual: '%s'", expected, actual)
+	}
 }
