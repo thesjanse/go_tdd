f82575fe22a5ab31e196b5e2e5a821a0830b7a68
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-27 23:43:32 +0300

    refactor: assert Search of an non-existing word

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -5,10 +5,24 @@ import "testing"
 func TestSearch(t *testing.T) {
 	t.Run("search for existing word", func(t *testing.T) {
 		d := Dictionary{"test": "simple test"}
-		actual := d.Search("test")
+		actual, _ := d.Search("test")
 		expected := "simple test"
 		assertStrings(t, actual, expected)
 	})
+
+	t.Run("search for unknown word", func(t *testing.T) {
+		d := Dictionary{"test": "simple test"}
+		_, actual := d.Search("unknown")
+		expected := ErrorWordNotFound
+		assertError(t, actual, expected)
+	})
+}
+
+func assertError(t testing.TB, actual, expected error) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("Actual '%q' error, but expected '%q'", actual, expected)
+	}
 }
 
 func assertStrings(t testing.TB, actual, expected string) {
