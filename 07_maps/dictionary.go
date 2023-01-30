427a2025eb81e2fa08c22b24db2f18fe9bf050f3
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-01-31 00:23:08 +0300

    feature: add Update method to the dictionary

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -5,6 +5,7 @@ type DictionaryErr string
 
 const (
 	ErrorWordNotFound      = DictionaryErr("Word not found")
+	ErrorWordDoesNotExist  = DictionaryErr("Word doesn't exist")
 	ErrorWordAlreadyExists = DictionaryErr("Word already exists")
 )
 
@@ -28,6 +29,19 @@ func (d Dictionary) AddWord(key, value string) error {
 		d[key] = value
 	case nil:
 		return ErrorWordAlreadyExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) UpdateWord(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrorWordNotFound:
+		return ErrorWordDoesNotExist
+	case nil:
+		d[key] = value
 	default:
 		return err
 	}
