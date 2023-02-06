f0a315fae3786876efec3c9294e0443341523130
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-02-06 22:33:43 +0300

    refactor: test the order of write/sleep calls

--- 09_mocking/mocking.go
+++ 09_mocking/mocking.go
@@ -7,16 +7,24 @@ import (
 	"time"
 )
 
+const sleep = "sleep"
+const write = "write"
+
 type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 type DefaultSleeper struct{}
--- 09_mocking/mocking_test.go
+++ 09_mocking/mocking_test.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
 	t.Run("Test single element print", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{}
-		Countdown(buffer, spySleeper)
+		Countdown(buffer, &SpyCountdownOperations{})
 
 		actual := buffer.String()
 		expected := `3
@@ -19,9 +19,16 @@ Go!`
 		if actual != expected {
 			t.Errorf("Actual: '%s'; expected: '%s'.", actual, expected)
 		}
+	})
 
-		if spySleeper.Calls != 3 {
-			t.Errorf("Actual calls: '%d', expected calls: '%d'", spySleeper.Calls, 3)
+	t.Run("Sleep after every write", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{write, sleep, write, sleep, write, sleep, write}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("Expected: '%v'; actual: '%v'", expected, spySleepPrinter.Calls)
 		}
 	})
 }
