fe44cddc138d7f32fce186e00f55c92fb24fcaf2
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-02-09 21:44:25 +0300

    feature: add channels for go routines

--- 10_concurrency/concurrency.go
+++ 10_concurrency/concurrency.go
@@ -1,18 +1,26 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultsChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultsChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
