0de7ddfef8cce927d2f3155d5731bb7d5f9aadbe
Alexander Mejinski <alexander.mejinski@gmail.com>
2023-02-11 22:33:23 +0300

    feature: testing with test servers

--- 11_select/select_test.go
+++ 11_select/select_test.go
@@ -1,12 +1,24 @@
 package selecto
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	expected := fastURL
 	actual := Racer(slowURL, fastURL)
@@ -14,4 +26,7 @@ func TestRacer(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Actual: '%s'; expected: '%s'.", actual, expected)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
